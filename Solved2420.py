76b5a6819d2ee97054436f4b0b9f7b54c6f97be3
806gw <shjea4364@naver.com>
2024-04-08 14:07:16 +0900

    update

---
+++ Solved2420.py
@@ -0,0 +1,4 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(abs(a - b))  # abs 함수는 안의 값의 절댓값을 계산하는 용도
--- Solved27866.py
+++ Solved27866.py
@@ -1,4 +1,3 @@
-A, B, C = map(int, input().split())     # A = 고정 비용, B = 가변 비용 C = 가격
-
-if C == A + B:
-    print()
+S = input()
+i = int(input())
+print(S[i - 1])
