179fdd878b3de1d624d72816c04fc2d7e84a37e1
806gw <shjea4364@naver.com>
2024-04-10 11:21:35 +0900

    solved2751

--- Solved2751.py
+++ Solved2751.py
@@ -1,8 +1,12 @@
+import sys
+input = sys.stdin.readline # 반복문으로 여러줄 입력받는 상황에서는 반드시 sys.stdin.readline()을 사용해야 시간초과가 발생하지 않습니다.
+
 n = int(input())
 arr = []
+
 for i in range(n):
     N = int(input())
-    arr.append(int)
+    arr.append(N)
 
-arr.sort()
-print(arr)
+for j in sorted(arr):
+    print(j)
