f5e1fc53fcb1688e4bd7a6a0eb28e55f93c60d07
806gw <shjea4364@naver.com>
2024-05-08 12:03:27 +0900

    update

---
+++ Solved8958.py
@@ -0,0 +1,5 @@
+a = int(input())
+for i in range(a):
+    b = input().split()
+
+print(b)
--- main.py
+++
@@ -1,7 +0,0 @@
-name = input()
-reverse_name = ''
-
-for c in name:
-    reverse_name = c + reverse_name
-
-print(reverse_name)
